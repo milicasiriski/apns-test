1d0257b16beff1458c67ff6eb6225a4538feeee9
Milica Siriski <milicasiriski@inviggo.com>
2023-07-31 17:02:52 +0200

    Initial Commit

---
+++ PushNotificationsTestApp/PushNotificationsTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationsTestAppApp.swift
+//  PushNotificationsTestApp
+//
+//  Created by Milica Siriski on 31.7.23..
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationsTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
